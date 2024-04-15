5b0bb8add8a7f81dd7756b8d5bcb9c6e7ff1515c
Adam Mazur <adam.mazur03@icloud.com>
2024-04-15 20:44:11 +0200

    tests and time measurement

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,46 +1,46 @@
-#include "Upload.hpp"
 #include "MergeSort.hpp"
 #include "QuickSort.hpp"
 #include "BucketSort.hpp"
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 int main() {
-    int size = 10; // Specify the size of the ranking array
-    MergeSort mergeSortObj(size);
-    QuickSort quickSortObj(size);
-    BucketSort bucketSortObj(size);
+    std::vector<int> sizes = {100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000}; // Specify the sizes of the ranking arrays
 
-    std::cout << "Dla " << size << " danych" << std::endl;
-    // Measure time for MergeSort
-    auto start = std::chrono::steady_clock::now();
-    //mergeSortObj.show();
-    mergeSortObj.sort(); // Sort the data
-    //mergeSortObj.show();
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> mergeSortTime = end - start;
+    for (int size : sizes) {
+        MergeSort mergeSortObj(size);
+        QuickSort quickSortObj(size);
+        BucketSort bucketSortObj(size);
 
-    std::cout << "MergeSort time: " << mergeSortTime.count() << " seconds" << std::endl;
+        std::cout << "For " << size << " data elements:" << std::endl;
 
-    // Measure time for QuickSort
-    start = std::chrono::steady_clock::now();
-    //quickSortObj.show();
-    quickSortObj.sort(); // Sort the data
-    //quickSortObj.show();
-    end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> quickSortTime = end - start;
+        // Measure time for MergeSort
+        auto start = std::chrono::steady_clock::now();
+        mergeSortObj.sort(); // Sort the data
+        auto end = std::chrono::steady_clock::now();
+        mergeSortObj.test();
+        std::chrono::duration<double> mergeSortTime = end - start;
+        std::cout << "MergeSort time: " << mergeSortTime.count() << " seconds" << std::endl;
 
-    std::cout << "QuickSort time: " << quickSortTime.count() << " seconds" << std::endl;
+        // Measure time for QuickSort
+        start = std::chrono::steady_clock::now();
+        quickSortObj.sort(); // Sort the data
+        end = std::chrono::steady_clock::now();
+        quickSortObj.test();
+        std::chrono::duration<double> quickSortTime = end - start;
+        std::cout << "QuickSort time: " << quickSortTime.count() << " seconds" << std::endl;
 
-    // Measure time for BucketSort
-    start = std::chrono::steady_clock::now();
-    //bucketSortObj.show();
-    bucketSortObj.sort(); // Sort the data
-    //bucketSortObj.show();
-    end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> bucketSortTime = end - start;
+        // Measure time for BucketSort
+        start = std::chrono::steady_clock::now();
+        bucketSortObj.sort(); // Sort the data
+        end = std::chrono::steady_clock::now();
+        bucketSortObj.test();
+        std::chrono::duration<double> bucketSortTime = end - start;
+        std::cout << "BucketSort time: " << bucketSortTime.count() << " seconds" << std::endl;
 
-    std::cout << "BucketSort time: " << bucketSortTime.count() << " seconds" << std::endl;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
